78443ea0461ab70fdc9359c09333af1bf922a378
Brandon Pham <pham-brandon@outlook.com>
2025-06-16 18:31:10 -0400

    set dark mode as default theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,30 +4,31 @@ import { useEffect, useState } from "react";
 import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
     useEffect(() => {
-        const storedTheme = localStorage.getItem("theme")
-        if (storedTheme === "dark") {
-            setIsDarkMode(true)
-            document.documentElement.classList.add("dark");
-        } else {
-            localStorage.setItem("theme", "light")
+        const storedTheme = localStorage.getItem("theme");
+        if (storedTheme === "light") {
             setIsDarkMode(false);
+            document.documentElement.classList.remove("dark");
+        } else {
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
+            document.documentElement.classList.add("dark");
         }
-    }, [])
+    }, []);
 
     const toggleTheme = () => {
         if (isDarkMode) {
             document.documentElement.classList.remove("dark");
-            localStorage.setItem("theme", "light")
+            localStorage.setItem("theme", "light");
             setIsDarkMode(false);
         } else {
             document.documentElement.classList.add("dark");
-            localStorage.setItem("theme", "dark")
+            localStorage.setItem("theme", "dark");
             setIsDarkMode(true);
         }
-    }
+    };
     
     return (
         <button 
@@ -41,6 +42,4 @@ export const ThemeToggle = () => {
             )}
         </button>
     );
-
-
 };
